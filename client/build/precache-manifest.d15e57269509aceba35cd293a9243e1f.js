64ac1d3fde50d09b34c90cfd39e49144eab055af
thip <thipsavanh@gmail.com>
2020-05-25 23:04:06 -0500

    added react app folders

---
+++ client/build/precache-manifest.d15e57269509aceba35cd293a9243e1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c667551daa30bdea4e396241dc79f4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da81b8ba8ed51bde9dff",
+    "url": "/static/css/main.2a4ecd36.chunk.css"
+  },
+  {
+    "revision": "b929237a33e20b6b49e6",
+    "url": "/static/js/2.bc5a5669.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bc5a5669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da81b8ba8ed51bde9dff",
+    "url": "/static/js/main.76cbc53c.chunk.js"
+  },
+  {
+    "revision": "25104bcb4b700f3b246b",
+    "url": "/static/js/runtime-main.9daa9d34.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
