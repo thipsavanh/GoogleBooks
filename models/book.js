aae5d5652e12f732bf1c00c4dd31223e99cb1a8d
thip <thipsavanh@gmail.com>
2020-05-26 01:46:54 -0500

    updated book.js model

--- models/book.js
+++ models/book.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  description: { type: String },
-  image: { type: String },
- link: { type: String },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  link: { type: String, required: true },
 });
 
 const Book = mongoose.model("Book", bookSchema);
